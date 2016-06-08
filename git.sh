74f77b6bccf975c5978aa520efc6d6a19eddd1d9
dchess <dc.hess@gmail.com>
2016-06-07 23:48:53 -0700

    Add git config shell script

---
+++ git.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt-get updare
+sudo apt-get install git
+
+name="$1"
+email="$2"
+
+git config --global core.editor "vim"
+git config --global user.name $name
+git config --global user.email $email
+
+cat << EOF >> ~/.gitconfig
+[alias]
+  co = checkout
+  cob = checkout -b
+  cm = !git add -A && git commit -m
+  ls = log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate
+  ll = log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat
+  fl = log -u
+  graph = log --graph --oneline --decorate --all
+  st = status -s
+EOF
