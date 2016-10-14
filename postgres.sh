511268528acf89bb0ca011a335e2c213818f0211
dchess <dc.hess@gmail.com>
2016-10-14 14:51:51 -0700

    Add install script for postgres db

---
+++ postgres.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+user="$1"
+
+sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install postgresql-common
+sudo apt-get install postgresql-9.5 libpq-dev
+
+sudo -u postgres createuser $user -s
