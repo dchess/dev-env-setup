0b28483639dc7b4c05e46783833cdfb78e96ceca
dchess <dc.hess@gmail.com>
2016-11-10 09:46:20 -0800

    Add assume yes to package installs

--- postgres.sh
+++ postgres.sh
@@ -5,7 +5,7 @@ user=$(whoami)
 sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
 wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install postgresql-common
-sudo apt-get install postgresql-9.5 libpq-dev
+sudo apt-get install -y postgresql-common
+sudo apt-get install -y postgresql-9.5 libpq-dev
 
 sudo -u postgres createuser $user -s
