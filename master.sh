25d9b55ceea15fea26521902c023d0931d5c8992
D.C. Hess <dc.hess@gmail.com>
2016-11-10 10:20:19 -0800

    Add Heroku toolbelt to install

--- master.sh
+++ master.sh
@@ -9,11 +9,15 @@
 # Install ruby, rails, and nodeJs
 ./rails.sh
 
+# Install Heroku Toolbelt
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
 # Install postgres database
 ./postgres.sh
 
 # Overwrite bash config
 cp -f .bashrc ~/.bashrc
-. ~/.bashrc
-
 echo "Environment is now configured for user"
+
+. ~/.bashrc
+reset
