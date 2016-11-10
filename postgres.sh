3e56e3ada22a6edf739359700d75917163d614d4
dchess <dc.hess@gmail.com>
2016-11-10 09:18:32 -0800

    Replace user arg with whoami in postgres script

--- postgres.sh
+++ postgres.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-user="$1"
+user=$(whoami)
 
 sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
 wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
