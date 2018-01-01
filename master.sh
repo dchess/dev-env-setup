4fdff22aaf6b0d0e4b20b6be5c4ef4a24b7538dc
dhess <daniel.hess@aspirepublicschools.org>
2017-12-31 18:38:48 -0800

    Add install of python3 virtualenv and remove rails install
    Moving away from rails work and shifting to python for now.

--- master.sh
+++ master.sh
@@ -2,15 +2,13 @@
 
 # Update system
 sudo apt-get update
+sudo apt-get install python3-venv
 
 # Install and configure VIM
 ./vim.sh
 
 # Configure git
 ./git.sh
-
-# Install ruby, rails, and nodeJs
-./rails.sh
 
 # Install Heroku Toolbelt
 wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
