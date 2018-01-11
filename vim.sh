ae43cb355690002704b9907e21aad13ad0d94a94
dhess <daniel.hess@aspirepublicschools.org>
2018-01-10 17:00:11 -0800

    Update comments

--- vim.sh
+++ vim.sh
@@ -14,6 +14,11 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # -NERDTree
 # -NERDCommenter
 # -CtrlP
+# -SimplyFold
+# -IndentPython
+# -Syntastic
+# -Vim-Flake8
+# -YouCompleteMe
 # -Vim ColorSchemes (with srcery installed by default)
 cp -f .vimrc ~/.vimrc
 
