96aeeaad2cb03473a0de47bd2db26cd1071881fd
daniel_hess <daniel.hess@aspirepublicschools.org>
2018-01-23 12:24:03 -0800

    Update postgres to load most recent version

--- postgres.sh
+++ postgres.sh
@@ -6,6 +6,6 @@ sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main'
 wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y postgresql-common
-sudo apt-get install -y postgresql-9.5 libpq-dev
+sudo apt-get install -y postgresql libpq-dev
 
 sudo -u postgres createuser $user -s
